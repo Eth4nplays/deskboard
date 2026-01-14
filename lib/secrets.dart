312c0d2f191e6a6875e958b163e63b4d6a4123db
Ethan-Desktop <eth4nplays@outlook.my>
2026-01-14 20:59:43 +0800

    Changed secrets

--- lib/secrets.dart
+++ lib/secrets.dart
@@ -1,5 +1,5 @@
-const String clientIdS = "CLIENT_ID_HERE";
-const String clientSecretS = "CLIENT_SECRET_HERE";
+const String clientIdS = "bdf5acf8148e4dac9431bbb4aa8d52c7";
+const String clientSecretS = "fc3f6f466aa549d1b9aa26cf5eadd678";
 
 // Get these values from your Spotify Developer Dashboard: https://developer.spotify.com/dashboard 
 // Create an app, set the Redirect URI to "http://127.0.0.1:8580/callback", and replace the placeholders above.
