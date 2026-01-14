65ca068873112851a5678c701f10591efee57dcb
Ethan-Desktop <eth4nplays@outlook.my>
2026-01-14 21:01:03 +0800

    Removed secrets.dart

--- lib/secrets.dart
+++
@@ -1,5 +0,0 @@
-const String clientIdS = "CLIENT_ID_HERE";
-const String clientSecretS = "CLIENT_SECRET_HERE";
-
-// Get these values from your Spotify Developer Dashboard: https://developer.spotify.com/dashboard 
-// Create an app, set the Redirect URI to "http://127.0.0.1:8580/callback", and replace the placeholders above.
